4cbe64839cb03ed8159b9c1f8bbf635f058c8995
Mihai <mihai@pop-os.localdomain>
2021-09-22 17:15:12 -0400

    added stages

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,6 +2,25 @@ node {
     //  instructions
     // parameter
     //Build trigger
-    stage("git pull") 
+    stage("git pull"){ 
         git "git@github.com:danfmihai/jenkins-pipeline.git"
+    }    
+
+    stage("Install apache"){
+        sh "ssh ec2-user@${DEVIP}  sudo yum install httpd -y"
+    }
+
+    stage("Start apache"){
+        sh "ssh ec2-user@${DEVIP} sudo systemctl start httpd"
+    }
+
+    stage("Copy files"){
+        sh "rsync -aP --delete index.html ec2-user@${DEVIP}:/tmp/"
+    }    
+
+    stage("Move files"){
+        sh "ssh ec2-user@${DEVIP}  sudo cp -f /tmp/index.html /var/www/html/index.html"
+    } 
+
+
 }
